666d6aa5cafc00e8833cc637e9762b6403ae3120
pstraw <github_main@pstrawberrie.live>
2025-06-19 12:47:32 -0500

    change starting skillPoints back to 250

--- MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
@@ -7,7 +7,7 @@ startingCash = 5000
 startingBank = 25000
 --startingBank = 100000
 --How many skill points a new characters start with
-skillPoints = 269
+skillPoints = 250
 
 professions = {
 	"combat_brawler",
