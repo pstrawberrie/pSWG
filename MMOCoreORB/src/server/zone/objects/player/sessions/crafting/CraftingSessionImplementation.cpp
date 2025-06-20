bb4f959656506cd35f951f6686a3acc57fdecb73
pstraw <github_main@pstrawberrie.live>
2025-06-20 11:01:32 -0500

    set crafting tool times to 1 second

--- MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
@@ -1387,11 +1387,13 @@ void CraftingSessionImplementation::createPrototype(int clientCounter, bool crea
 		int xp = manufactureSchematic->getDraftSchematic()->getXpAmount();
 
 		if (createItem) {
-			startCreationTasks(manufactureSchematic->getComplexity() * 2, false);
+			// startCreationTasks(manufactureSchematic->getComplexity() * 2, false);
+			startCreationTasks(1, false);
 
 		} else {
 			// This is for practicing
-			startCreationTasks(manufactureSchematic->getComplexity() * 2, true);
+			// startCreationTasks(manufactureSchematic->getComplexity() * 2, true);
+			startCreationTasks(1, true);
 			xp = round(xp * 1.05f);
 		}
 
