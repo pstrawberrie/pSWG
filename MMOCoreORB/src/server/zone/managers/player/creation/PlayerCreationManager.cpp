53cae770904b896dfa74ba211cea17079ed446b3
pstraw <github_main@pstrawberrie.live>
2025-06-23 14:55:28 -0500

    broadcast message when new player joins

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -567,6 +567,13 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
 	box->setPromptTitle("PLEASE NOTE");
 	box->setPromptText("You are limited to creating one character per hour. Attempting to create another character or deleting your character before the 1 hour timer expires will reset the timer.");
 
+	// Broadcast new players joining
+	String playerFirstName = playerCreature->getFirstName();
+	String playerLastName = playerCreature->getFirstName();
+	StringBuffer zBroadcast;
+	zBroadcast << "\\#00ace6" << playerFirstName << " " << playerLastName << " \\#ffb90f has entered the galaxy";
+	playerCreature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
+
 	ghost->addSuiBox(box);
 	playerCreature->sendMessage(box->generateMessage());
 
