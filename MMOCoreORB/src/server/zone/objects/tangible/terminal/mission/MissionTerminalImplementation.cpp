b29fc67626901169e7eb95b4260c87f01aeb78c1
pstraw <github_main@pstrawberrie.live>
2025-06-23 16:19:14 -0500

    add missed code for new mission terminal radial menu options to work in MissionTerminalImplementation.cpp

--- MMOCoreORB/src/server/zone/objects/tangible/terminal/mission/MissionTerminalImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/terminal/mission/MissionTerminalImplementation.cpp
@@ -29,6 +29,13 @@ void MissionTerminalImplementation::fillObjectMenuResponse(ObjectMenuResponse* m
 		menuResponse->addRadialMenuItemToRadialID(73, 76, 3, "@city/city:south"); // South
 		menuResponse->addRadialMenuItemToRadialID(73, 77, 3, "@city/city:west"); // West
 	}
+
+	// destroy-mission-tools: start addition
+	if (terminalType == "general" || terminalType == "imperial" || terminalType == "rebel") {
+		menuResponse->addRadialMenuItem(112, 3, "Choose Mission Direction");
+		menuResponse->addRadialMenuItem(113, 3, "Choose Mission Level");
+	}
+	// destroy-mission-tools: end addition
 }
 
 int MissionTerminalImplementation::handleObjectMenuSelect(CreatureObject* player, byte selectedID) {
@@ -79,7 +86,8 @@ int MissionTerminalImplementation::handleObjectMenuSelect(CreatureObject* player
 		cityManager->alignAmenity(city, player, _this.getReferenceUnsafeStaticCast(), selectedID - 74);
 
 		return 0;
-	} else if (selectedID == 112) {
+	} // destroy-mission-tools: start addition
+	else if (selectedID == 112) {
 
 		Lua* lua = DirectorManager::instance()->getLuaInstance();
 		if (lua == nullptr) {
@@ -113,6 +121,7 @@ int MissionTerminalImplementation::handleObjectMenuSelect(CreatureObject* player
 
 		return 0;
 	}
+	// destroy-mission-tools: end addition
 
 	return TangibleObjectImplementation::handleObjectMenuSelect(player, selectedID);
 }
