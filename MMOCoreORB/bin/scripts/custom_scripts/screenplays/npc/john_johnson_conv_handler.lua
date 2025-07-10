2c0a09765434df6e3911c8824a6ae1e838d8ab44
pstraw <github_main@pstrawberrie.live>
2025-07-10 15:05:21 -0500

    more debugging

--- MMOCoreORB/bin/scripts/custom_scripts/screenplays/npc/john_johnson_conv_handler.lua
+++ MMOCoreORB/bin/scripts/custom_scripts/screenplays/npc/john_johnson_conv_handler.lua
@@ -1,17 +1,22 @@
-print("[DEBUG] Loaded johnJohnsonConvoHandler")
-
 johnJohnsonConvoHandler = conv_handler:new {}
 
 function johnJohnsonConvoHandler:getInitialScreen(pPlayer, pNpc, pConvTemplate)
 	print("[DEBUG] johnJohnsonConvoHandler:getInitialScreen called")
 	local convoTemplate = LuaConversationTemplate(pConvTemplate)
-	return convoTemplate:getScreen("greeting")
+	local greetingScreen = convoTemplate:getScreen("greeting")
+	if greetingScreen == nil then
+		print("[DEBUG] getScreen('greeting') returned nil!")
+	else
+		print("[DEBUG] getScreen('greeting') returned a valid screen.")
+	end
+	return greetingScreen
 end
 
 function johnJohnsonConvoHandler:runScreenHandlers(pConvTemplate, pPlayer, pNpc, selectedOption, pConvScreen)
 	print("[DEBUG] johnJohnsonConvoHandler:runScreenHandlers called")
 	local screen = LuaConversationScreen(pConvScreen)
 	local screenID = screen:getScreenID()
+	print("[DEBUG] runScreenHandlers screenID:", screenID)
 	local pConvScreen = screen:cloneScreen()
 	-- local clonedConversation = LuaConversationScreen(pConvScreen) -- Not needed
 
