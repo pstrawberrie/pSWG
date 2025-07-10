e89f8026a873bbd1b893decd30be6be07380e70a
pstraw <github_main@pstrawberrie.live>
2025-07-10 15:42:08 -0500

    add example_npc for future reference of a working npc, re-organize custom_scripts directories once again to keep things clean

--- MMOCoreORB/bin/scripts/custom_scripts/mobile/serverobjects.lua
+++ MMOCoreORB/bin/scripts/custom_scripts/mobile/serverobjects.lua
@@ -4,10 +4,10 @@
 --   NPCs
 -- ######################
 
--- John Johnson
-includeFile("../custom_scripts/mobile/john_johnson.lua")
-includeFile("../custom_scripts/mobile/conversations/npc/john_johnson_convo.lua")
-
 -- Example NPC
-includeFile("../custom_scripts/mobile/example_npc.lua")
-includeFile("../custom_scripts/mobile/conversations/npc/example_npc_convo.lua")
+includeFile("../custom_scripts/mobile/npc/example_npc.lua")
+includeFile("../custom_scripts/mobile/conversations/example_npc_convo.lua")
+
+-- John Johnson
+includeFile("../custom_scripts/mobile/npc/john_johnson.lua")
+includeFile("../custom_scripts/mobile/conversations/john_johnson_convo.lua")
--- MMOCoreORB/bin/scripts/custom_scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/custom_scripts/screenplays/screenplays.lua
@@ -12,10 +12,11 @@ includeFile("../custom_scripts/screenplays/tools/mission_level_choice.lua")
 --   NPCs
 -- ######################
 
--- John Johnson
-includeFile("../custom_scripts/screenplays/npc/john_johnson.lua")
-includeFile("../custom_scripts/screenplays/npc/john_johnson_conv_handler.lua")
-
 -- Example NPC
 includeFile("../custom_scripts/screenplays/npc/example_npc.lua")
-includeFile("../custom_scripts/screenplays/npc/example_npc_conv_handler.lua")
+includeFile("../custom_scripts/screenplays/conversations/example_npc_conv_handler.lua")
+
+-- John Johnson
+includeFile("../custom_scripts/screenplays/npc/john_johnson.lua")
+includeFile("../custom_scripts/screenplays/conversations/john_johnson_conv_handler.lua")
+
