099d81f8e7977d08866e3d41e061a3dbe2166a7b
pstraw <github_main@pstrawberrie.live>
2025-07-19 07:59:01 -0500

    add color picker to the rest of armors, remove from ubese gloves

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_utility_belt.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_utility_belt.lua
@@ -43,6 +43,7 @@
 
 object_tangible_wearables_armor_stormtrooper_armor_stormtrooper_utility_belt = object_tangible_wearables_armor_stormtrooper_shared_armor_stormtrooper_utility_belt:new {
 	templateType = ARMOROBJECT,
+	objectMenuComponent = "ArmorObjectMenuComponent",
 	faction = "Imperial",
 	playerRaces = { "object/creature/player/bothan_male.iff",
 				"object/creature/player/bothan_female.iff",
